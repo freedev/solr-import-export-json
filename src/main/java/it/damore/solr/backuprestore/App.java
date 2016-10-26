a1655306d031b5280fa50b918b20c6ad203fa7f3
Vincenzo D'Amore <v.damore@gmail.com>
2016-10-26 23:56:25 +0200

    added gpl license

--- src/main/java/it/damore/solr/backuprestore/App.java
+++ src/main/java/it/damore/solr/backuprestore/App.java
@@ -38,6 +38,24 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 import it.damore.solr.backuprestore.Config.ActionType;
 
+/*
+    This file is part of solr-backup-restore-json.
+
+    solr-backup-restore-json is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    solr-backup-restore-json is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with solr-backup-restore-json.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 /**
  * @author freedev
  *
--- src/main/java/it/damore/solr/backuprestore/StreamUtils.java
+++ src/main/java/it/damore/solr/backuprestore/StreamUtils.java
@@ -4,6 +4,24 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collector;
 
+/*
+This file is part of solr-backup-restore-json.
+
+solr-backup-restore-json is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+solr-backup-restore-json is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with solr-backup-restore-json.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 public class StreamUtils {
 
     /**
