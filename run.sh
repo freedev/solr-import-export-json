e8b7674244eb4941c6bf087376b26ec2bc5913e0
Vincenzo D'Amore <v.damore@gmail.com>
2020-04-14 00:06:02 +0200

    fixed script not accepting parameters with spaces

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PARAMS="$*"
+PARAMS=""
 
-if [ "A$PARAMS" == "A" ]
+if [ "A$@" == "A" ]
 then
   PARAMS="--help"
 fi
 
 export JAVA_OPTS=-Dfile.encoding=utf-8
 
-java $JAVA_OPTS -jar target/solr-import-export-1.0.jar $PARAMS
+java $JAVA_OPTS -jar target/solr-import-export-1.0.jar "$@" $PARAMS
